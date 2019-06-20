b361bd54658ff9cb3842dc90e1e442daa2835d68
krikaz <pascal.bou@protonmail.com>
2019-06-20 13:58:18 +0200

    create one actionType and one reducer

---
+++ friends/src/state/actionTypes.js
@@ -0,0 +1,2 @@
+// export const LOGIN = 'LOGIN';
+export const GET_ALL_FRIENDS = 'GET_ALL_FRIENDS';
---
+++ friends/src/state/reducers.js
@@ -0,0 +1,14 @@
+import * as types from './actionTypes';
+
+const initialState = {
+	friends: [],
+};
+
+export const getAllFriendsReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case types.GET_ALL_FRIENDS:
+			return { ...state, friends: action.payload };
+		default:
+			return state;
+	}
+};
