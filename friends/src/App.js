43068462359148658a5625a160f01aa5bd1982b0
krikaz <pascal.bou@protonmail.com>
2019-06-20 15:50:37 +0200

    fetch friends button now works, displaying friends names

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actionsCreators from './actions';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
-import Friends from './components/Friends';
+// import Friends from './components/Friends';
 import Login from './components/Login';
 
 class App extends React.Component {
 	render() {
+		const friends = this.props.friends;
 		return (
 			<BrowserRouter>
 				<Route
@@ -14,7 +15,16 @@ class App extends React.Component {
 					path="/"
 					render={props => {
 						if (true) {
-							return <Friends props={props} />;
+							return (
+								<div>
+									{friends.map(friend => (
+										<div key={friend.id}>{friend.name}</div>
+									))}
+									<button onClick={this.props.fetchFriends}>
+										Fetch Friends
+									</button>
+								</div>
+							);
 						}
 						return <Redirect to="login" />;
 					}}
