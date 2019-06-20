7a596465848da6e86986fbccf32b7ed297a3cade
krikaz <pascal.bou@protonmail.com>
2019-06-20 14:04:27 +0200

    created store, wrapped App in Provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
+const store = createStore(rootReducer, compose(applyMiddleware(thunk, logger)));
 
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
--- friends/src/reducers/reducers.js
+++ friends/src/reducers/reducers.js
@@ -1,4 +1,5 @@
-import * as types from './actionTypes';
+import * as types from '../actions/actionTypes';
+
 
 const initialState = {
 	friends: [],
