cf9f1af1a7b6b1f11b89cb0d808bbbd36e6a1e1f
krikaz <pascal.bou@protonmail.com>
2019-06-20 16:36:27 +0200

    added routing to rootpage after login success

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -5,13 +5,21 @@ import { login } from '../actions';
 export class Login extends React.Component {
 	userRef = React.createRef();
 
-	passRef = React.createRef();
+  passRef = React.createRef();
+  
+  checkingToken = () => {
+    if (localStorage.getItem('token')) {
+			this.props.history.push('/');
+		}
+  }
 
 	onLogin = () => {
 		const username = this.userRef.current.value;
 		const password = this.passRef.current.value;
 		// console.log(username, password);
 		this.props.login(username, password);
+
+		window.setTimeout(this.checkingToken, 1500);
 	};
 
 	render() {
