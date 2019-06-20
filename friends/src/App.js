83f226e23e26f1d113bc66f26ad72c7e4137c1e0
krikaz <pascal.bou@protonmail.com>
2019-06-20 14:17:47 +0200

    mapped state to props and connected component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import * as actionsCreators from './actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<button />
+			</div>
+		);
+	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return {
+		friends: state.getAllFriendsReducer.friends,
+	};
+}
+
+export default connect (
+  mapStateToProps,
+  actionsCreators
+)(App);
